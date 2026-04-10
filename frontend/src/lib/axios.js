4ff453717ac0915c6fe85d017a221c3821c081dd
Lab Sadik <emailme.sadik@gmail.com>
2026-04-10 14:19:26 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://fullstack-chat-app-4gl1.onrender.com/api" : "/api",
   withCredentials: true,
 });
